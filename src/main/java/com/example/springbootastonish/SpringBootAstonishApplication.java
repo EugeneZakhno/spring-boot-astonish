3edac247153271472d532ce79b561db8b6f4354a
Eugene <30446748+EugeneZakhno@users.noreply.github.com>
2024-10-13 22:31:14 +0300

    init

---
+++ src/main/java/com/example/springbootastonish/SpringBootAstonishApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootastonish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAstonishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAstonishApplication.class, args);
+    }
+
+}
